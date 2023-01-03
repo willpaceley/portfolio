fb99d8cf369a091146cc6c0479a8cf29b0f95442
Will Paceley <pace0033@algonquinlive.com>
2023-01-03 10:31:15 -0500

    add favicon

--- js/main.js
+++ js/main.js
@@ -2,6 +2,10 @@
 // Or maybe tic tac toe?
 
 function init() {
+  updateCopyright();
+}
+
+function updateCopyright() {
   const copyright = document.querySelector('.copyright');
   const year = new Date().getFullYear();
   copyright.textContent = `Copyright © ${year} Will Paceley`;
